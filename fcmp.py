0c7421f83803a08e305ec67ffcb787426466ff07
snorlax007 <138244682+snorlax007@users.noreply.github.com>
2023-11-23 01:02:45 +0530

    Update fcmp.py

--- fcmp.py
+++ fcmp.py
@@ -1,14 +1,17 @@
 import sys
-
+#filename from first argument is taken and opened in read mode
 f = open(sys.argv[1],"r")
 file1 = f.read()
 f.close()
+
+# second file taken in the same way
 g = open(sys.argv[2],"r")
 file2 = g.read()
 g.close()
 print(file1) 
 print(file2)
- 
+
+ #comparing both files
 if file1 == file2 :
     print(1)
 else :
