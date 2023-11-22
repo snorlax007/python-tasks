417e604b71224ab599455e10f3f0dad0279c5c13
snorlax007 <138244682+snorlax007@users.noreply.github.com>
2023-11-23 01:05:27 +0530

    Update lang.py

--- lang.py
+++ lang.py
@@ -13,14 +13,19 @@
 
 translate()    
 """
-
+#translate function to add 'o' between consonants
 def translate(str) :
+    #taking vowels inside a string
     vowel= "aeiou "
     a =""
+    #empty string a to generate the result
     for i in str:
        if i in vowel:
+           #if the letter is a vowel then add it as it is
           a = a + i
        else:
+           #else add letter 'o' in between
           a = a + i + "o" + i   
     print(a)
+    #extra space is added in vowel funtion to take care of spaces in the string
 translate("this is fun")          
